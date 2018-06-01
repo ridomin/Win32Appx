d41d45f8b8886b78cdc4a3217325c2be6a71d295
ridomin <ridomin@live.com>
2018-05-31 22:18:31 -0700

    add resources in wapproj

--- WinForms461/Form1.cs
+++ WinForms461/Form1.cs
@@ -21,7 +21,11 @@ namespace WinForms461
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            ShowAppInfo();
+        }
 
+        private void ShowAppInfo()
+        {
             textBox1.Text = $"ImageRuntime: {Assembly.GetExecutingAssembly().ImageRuntimeVersion} {Environment.NewLine}";
             textBox1.Text += $"WinForms: {this.FormBorderStyle.GetType().Assembly.Location} {Environment.NewLine}";
             textBox1.Text += $"ThisApp: {Assembly.GetExecutingAssembly().Location} {Environment.NewLine}";
@@ -32,6 +36,25 @@ namespace WinForms461
         string GetArch()
         {
             return IntPtr.Size == 4 ? "x86" : "x64";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("es-es");
+            ShowAppInfo();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-eu");
+            ShowAppInfo();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("it-it");
+            ShowAppInfo();
         }
     }
 }
